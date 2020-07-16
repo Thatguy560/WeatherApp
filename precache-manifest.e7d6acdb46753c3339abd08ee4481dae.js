a25d2b9cf85334a86ce675a1305bd131bbfb4986
Peter Stevens <Peterstevens510@gmail.com>
2020-07-16 13:56:37 +0100

    Updates

---
+++ precache-manifest.e7d6acdb46753c3339abd08ee4481dae.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e44abad39482c3becce1ef680bb57f4",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "cd2f039be5e1a3568387",
+    "url": "/WeatherApp/static/css/main.7ce380c3.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/WeatherApp/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2f039be5e1a3568387",
+    "url": "/WeatherApp/static/js/main.eadd5a77.chunk.js"
+  },
+  {
+    "revision": "ff4aa2a48c5d60d03858",
+    "url": "/WeatherApp/static/js/runtime-main.add8d4cb.js"
+  },
+  {
+    "revision": "6eea120c4e66afc8228121ce30447f1b",
+    "url": "/WeatherApp/static/media/ClearSkies.6eea120c.jpg"
+  },
+  {
+    "revision": "f2b30db3c8bc87486f5b452dd6e6d300",
+    "url": "/WeatherApp/static/media/Cold.f2b30db3.jpg"
+  },
+  {
+    "revision": "97b15813e61a68e90140dcb34dd90d3d",
+    "url": "/WeatherApp/static/media/Coldclear.97b15813.jpg"
+  },
+  {
+    "revision": "7c8a880292e715dbc9ba224eeea17f70",
+    "url": "/WeatherApp/static/media/Hot.7c8a8802.jpg"
+  },
+  {
+    "revision": "dbe433c1110e964cfd84ff27e0fadf8e",
+    "url": "/WeatherApp/static/media/Ice.dbe433c1.jpg"
+  },
+  {
+    "revision": "933c6303bbc176b65ba7568ba062980f",
+    "url": "/WeatherApp/static/media/Lightning.933c6303.jpg"
+  },
+  {
+    "revision": "b0342d067ef0f1d0ed3fd7c8c7d76d96",
+    "url": "/WeatherApp/static/media/Raining.b0342d06.jpg"
+  },
+  {
+    "revision": "9ec5d28872c83a7f863db8a15e7df911",
+    "url": "/WeatherApp/static/media/Snow.9ec5d288.jpg"
+  },
+  {
+    "revision": "c925f08e55b1839520e2af7751959631",
+    "url": "/WeatherApp/static/media/Warm.c925f08e.jpg"
+  },
+  {
+    "revision": "cad960371c29363af98081bf4b0db847",
+    "url": "/WeatherApp/static/media/WarmCloudy.cad96037.jpg"
+  },
+  {
+    "revision": "8baf47ae97b1394757b21f7d86472409",
+    "url": "/WeatherApp/static/media/backgroundwallpaper.8baf47ae.jpg"
+  }
+]);
